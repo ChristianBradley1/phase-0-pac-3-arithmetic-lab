91c9d444bf780320ac7ec7d1a81d64f444caf5d7
Christian <bradleycb14@gmail.com>
2021-10-02 18:27:06 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(x, y){
+    return x + y;
+}
+
+function subtract(x, y){
+    return x - y;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(x, y){
+    return x / y;
+}
+
+function increment(n){
+    return ++n;
+}
+
+function decrement(n){
+    return --n;
+}
+
+function makeInt(a){
+   return parseInt(a, 10);
+    
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
